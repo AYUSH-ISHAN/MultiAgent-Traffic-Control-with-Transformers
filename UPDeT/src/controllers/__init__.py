b4166affb82273f111d2365e01ce715b961cbee4
Ayush Ishan <77822265+AYUSH-ISHAN@users.noreply.github.com>
2022-06-14 14:10:45 +0530

    Delete UPDeT directory

--- UPDeT/src/controllers/__init__.py
+++
@@ -1,5 +0,0 @@
-REGISTRY = {}
-
-from .basic_controller import BasicMAC
-
-REGISTRY["basic_mac"] = BasicMAC
--- UPDeT/src/learners/__init__.py
+++
@@ -1,9 +0,0 @@
-from .q_learner import QLearner
-from .coma_learner import COMALearner
-from .qtran_learner import QLearner as QTranLearner
-
-REGISTRY = {}
-
-REGISTRY["q_learner"] = QLearner
-REGISTRY["coma_learner"] = COMALearner
-REGISTRY["qtran_learner"] = QTranLearner
